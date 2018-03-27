4b4b9f53f0051defba3f515a8da94c58522bb04a
林柏億 <istar0me@gmail.com>
2018-03-28 00:34:22 +0800

    finish 5-4

--- C5/MyGame/GameScene.swift
+++ C5/MyGame/GameScene.swift
@@ -9,6 +9,9 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var button1 = SKSpriteNode(imageNamed: "button1.png")
+    var button2 = SKSpriteNode(imageNamed: "button1.png")
+    
     override func didMove(to view: SKView) {
         /* 設定遊戲的場景 */
         // 1. 版權宣告標籤
@@ -25,8 +28,7 @@ class GameScene: SKScene {
         bgImage.position = CGPoint(x:self.frame.midX, y:self.frame.midY);
         
         // 3. 選單按鈕
-        //    About Us 按鈕
-        let button1 = SKSpriteNode(imageNamed: "button1.png")
+        //let button1 = SKSpriteNode(imageNamed: "button1.png")
         self.addChild(button1)
         button1.position = CGPoint(x: 300,y:200 );
         
@@ -37,8 +39,7 @@ class GameScene: SKScene {
         myLabel1.position = CGPoint(x: 300,y:200-12 );
         self.addChild(myLabel1)
         
-        //    Start 按鈕
-        let button2 = SKSpriteNode(imageNamed: "button1.png")
+        //let button2 = SKSpriteNode(imageNamed: "button1.png")
         self.addChild(button2)
         button2.position = CGPoint(x: 300,y:300 );
         
@@ -50,28 +51,61 @@ class GameScene: SKScene {
         self.addChild(myLabel2)
         
     }
+    
+    /* 手指頭移動的觸發事件 */
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    /* 手指頭放開的觸發事件 */
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        button1.texture=SKTexture(imageNamed: "button1.png")
+        button2.texture=SKTexture(imageNamed: "button1.png")
+    }
+    
     /* 點下去的觸發事件 */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        for touch: AnyObject in touches {
-            let location = touch.location(in: self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotate(byAngle: CGFloat(M_PI), duration:1)
-            
-            sprite.run(SKAction.repeatForever(action))
-            
-            self.addChild(sprite)
+        let touch = touches.first
+        
+        let location: CGPoint = touch!.location(in: self) // 取得按下的位置
+        if button2.contains(location) { // 判斷是否點擊到按鈕
+            button2.texture=SKTexture(imageNamed: "button2.png")
         }
+        
+        if button1.contains(location) { // 判斷是否點擊到按鈕
+            button1.texture=SKTexture(imageNamed: "button2.png")
+        }
+        
+        
+        //        let buttonx = SKSpriteNode(imageNamed: "button1.png")
+        //        self.addChild(buttonx)
+        //        button2.position = location
+        
+        
+        
+        /* 原本點擊後會顯示飛機的程式 */
+        /*
+         for touch: AnyObject in touches {
+         let location = touch.locationInNode(self)
+         
+         let sprite = SKSpriteNode(imageNamed:"Spaceship")
+         
+         sprite.xScale = 0.5
+         sprite.yScale = 0.5
+         sprite.position = location
+         
+         let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+         
+         sprite.runAction(SKAction.repeatActionForever(action))
+         
+         self.addChild(sprite)
+         }*/
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
         /* Called before each frame is rendered */
     }
 }
-
