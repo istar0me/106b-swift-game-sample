200bcf306a2d00abbe70d508dc0c108a5e28a3b9
林柏億 <istar0me@gmail.com>
2018-04-11 15:59:28 +0800

    add C7 first edition (7.0)

---
+++ C7/MyScore/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  MyScore
+//
+//  Created by Powen Ko on 2/19/15.
+//  Copyright (c) 2015 Powen Ko. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+    }
+   
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
