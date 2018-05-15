b98031900b721d07abc44cd9123f938bfbab29c1
林柏億 <istar0me@gmail.com>
2018-05-15 11:45:59 +0800

    10-1(before)

---
+++ C10/10-1/MyMusic/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  MyMusic
+//
+//  Created by Powen Ko on 2/25/15.
+//  Copyright (c) 2015 Powen Ko. All rights reserved.
+//
+import UIKit
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
