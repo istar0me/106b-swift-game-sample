986e7a361b4c74118e3d89d61929c1864d834ef1
林柏億 <istar0me@gmail.com>
2018-05-22 11:37:26 +0800

    finish 14-3-1

--- C14/MyFly/GameScene.swift
+++ C14/MyFly/GameScene.swift
@@ -9,6 +9,12 @@ import SpriteKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     let myScore = SKLabelNode(fontNamed:"AmericanTypewriter-Bold")
     var fly:SKSpriteNode = SKSpriteNode()
+    // 指定物件的種類
+    let flyCat:UInt32 = 1 << 0      //1 設定為主角的種類
+    let pipeCat:UInt32 = 1 << 1     //2 設定為水管的種類
+    let levelCat:UInt32 = 1 << 2    //4 設定為地板的種類
+    let scoreCat:UInt32 = 1 << 3    //8 設定為成績的種類
+    
     override func didMove(to view: SKView) {
         /* 設定遊戲的場景 */
         // 1. 版權宣告標籤
@@ -35,7 +41,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(bgImage)
         
         
-        // 4. 主角
+        FunSet_Fly()
+        self.physicsWorld.gravity = CGVector(dx: 0.0,dy: -5.0) // 地心引力的強度（在此為向下 5）
+        self.physicsWorld.contactDelegate = self // 物理反應的觸發類別，在此為自己（GameScene 類別）
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        /* Called before each frame is rendered */
+    }
+    
+    func FunSet_Fly(){
         let fly1 = SKTexture(imageNamed: "fly1.png") // 主角圖片 1
         let fly2 = SKTexture(imageNamed: "fly2.png") // 主角圖片 2
         let ani1 = SKAction.animate(with: [fly1,fly2], timePerFrame:0.25) // 0.25 秒切換圖片
@@ -50,17 +65,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         fly.physicsBody = SKPhysicsBody(rectangleOf: fly.size) // 設定物理大小為實際圖片的大小
         fly.physicsBody?.isDynamic = true // 設定動態物理計算，代表此物件會受到物理反應改變為自動改變位置（在此為往下掉）
         fly.physicsBody?.allowsRotation = false // 執行物理反應時，是否會旋轉（在此設定否）
-        self.physicsWorld.gravity = CGVector(dx: 0.0,dy: -5.0) // 地心引力的強度（在此為向下 5）
-        self.physicsWorld.contactDelegate = self // 物理反應的觸發類別，在此為自己（GameScene 類別）
-    }
-    
-    // 點擊後向上跳躍
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.fly.physicsBody?.velocity=CGVector(dx: 0,dy: 0) // 設定物件的重力加速度，在此為 0
-        self.fly.physicsBody?.applyImpulse(CGVector(dx: 0,dy: 200)) // 向上推動 200
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        /* Called before each frame is rendered */
+        fly.physicsBody?.categoryBitMask = flyCat // 設定物件的種類為 flyCat
+        fly.physicsBody?.collisionBitMask = levelCat | pipeCat // 設定物件會被碰撞的種類有 levelCat 和 PipeCat
+        fly.physicsBody?.contactTestBitMask = levelCat | pipeCat // 指定不會被穿透的物件
     }
 }
