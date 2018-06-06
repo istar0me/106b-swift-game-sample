240461676b69170e6fdcd61f7cf28a46590e0b1a
林柏億 <istar0me@gmail.com>
2018-06-06 11:25:59 +0800

    finish 15-6-2

--- C15/MySpace/SpaceNode.swift
+++ C15/MySpace/SpaceNode.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SpriteKit
 
-class SpaceNode: SKSpriteNode{
+class SpaceNode: SKSpriteNode, SKPhysicsContactDelegate {
     var Steps:[CGPoint]=[] // 紀錄路徑的陣列
     init(imageNamed name: String) { // 初始化
         let texture = SKTexture(imageNamed: name)
@@ -31,6 +31,7 @@ class SpaceNode: SKSpriteNode{
         // print(step.x)
     }
     
+    var velocity = CGPoint(x: 0, y: 0)
     let POINTS_PER_SEC: CGFloat = 80.0 // 設定每秒移動的點(Dot)數
     // 移動飛碟
     func move(_ timer:TimeInterval){
@@ -42,7 +43,7 @@ class SpaceNode: SKSpriteNode{
             let offset = CGPoint(x: targetPoint.x - currentPosition.x, y: targetPoint.y - currentPosition.y) // 分別計算 x, y 軸的位移
             let length = Double(sqrtf(Float(offset.x * offset.x) + Float(offset.y * offset.y))) // Sqrt(x^2+y^2)
             let direction = CGPoint(x:CGFloat(offset.x) / CGFloat(length), y: CGFloat(offset.y) / CGFloat(length))
-            let velocity = CGPoint(x: direction.x * POINTS_PER_SEC, y: direction.y * POINTS_PER_SEC)
+            velocity = CGPoint(x: direction.x * POINTS_PER_SEC, y: direction.y * POINTS_PER_SEC)
             
             newPosition = CGPoint(x:currentPosition.x + velocity.x * CGFloat(timer), y:currentPosition.y + velocity.y * CGFloat(timer))
             
@@ -50,6 +51,9 @@ class SpaceNode: SKSpriteNode{
             if frame.contains(targetPoint) { // 判斷是否已經到目標的位置
                 Steps.remove(at: 0) // 移除矩陣資料
             }
+        } else {
+            let newPosition = CGPoint(x:currentPosition.x + velocity.x * CGFloat(timer), y:currentPosition.y + velocity.y * CGFloat(timer))
+            position = newPosition
         }
     }
     
